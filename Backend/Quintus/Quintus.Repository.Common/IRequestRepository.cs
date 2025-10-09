badbbf3e73380ba56aa643f2fa073de0dafb4e2c
filip7557 <filip.curic@student.ferit.hr>
2025-10-09 18:48:55 +0200

    Repository and DTO

---
+++ Backend/Quintus/Quintus.Repository.Common/IRequestRepository.cs
@@ -0,0 +1,11 @@
+﻿using Quintus.Model.Entities;
+
+namespace Quintus.Repository.Common
+{
+    public interface IRequestRepository
+    {
+        Task<bool> AddRequestAsync(Request request);
+        Task<Request?> GetRequestByIdAsync(Guid id);
+        Task<IEnumerable<Request>> GetAllRequestsAsync();
+    }
+}
