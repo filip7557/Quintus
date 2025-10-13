1a4937245d1ca07f598357ef4efd280607f20059
filip7557 <filip.curic@student.ferit.hr>
2025-10-13 22:47:58 +0200

    Add exceptions

---
+++ Backend/Quintus/Quintus.Common/Exceptions/DuplicateUserException.cs
@@ -0,0 +1,9 @@
+﻿namespace Quintus.Common.Exceptions
+{
+    public class DuplicateUserException : Exception
+    {
+        public DuplicateUserException() : base("A user with the provided email already exists.")
+        {
+        }
+    }
+}
---
+++ Backend/Quintus/Quintus.Common/Exceptions/InvalidPasswordException.cs
@@ -0,0 +1,13 @@
+﻿namespace Quintus.Common.Exceptions
+{
+    public class InvalidPasswordException : Exception
+    {
+        public InvalidPasswordException(string message) : base(message)
+        {
+        }
+
+        public InvalidPasswordException() : base("The provided password does not meet the required criteria.")
+        {
+        }
+    }
+}
