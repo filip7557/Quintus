ade8060cc57e43bbd266c38f8c96f166461af519
Filip Ćurić <filip.curic6@yahoo.com>
2026-08-10 15:28:54 +0200

    Fix wrong url in config

--- Frontend/quintus/next.config.mjs
+++ Frontend/quintus/next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
                 protocol: 'https',
                 hostname: 'www.instalacije-quintus.hr',
                 pathname: '/**',
-            }
+            },
             {
                 protocol: 'https',
                 hostname: 'res.cloudinary.com',
