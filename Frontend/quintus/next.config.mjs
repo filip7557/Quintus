02fafbe6564bb2d83f13087f07b70211efb30f20
Filip Ćurić <filip.curic6@yahoo.com>
2026-08-31 15:49:20 +0200

    Add more data to service SEO

--- Frontend/quintus/next.config.mjs
+++ Frontend/quintus/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
+    allowedDevOrigins: ['192.168.1.5'],
     experimental: {
         inlineCss: true,
     },
--- Frontend/quintus/src/lib/serviceSeo.js
+++ Frontend/quintus/src/lib/serviceSeo.js
@@ -23,6 +23,33 @@ const SERVICE_SEO_OVERRIDES = {
       "certificirani serviseri klima uređaja",
       "certificirani serviseri klima uređaja Našice",
     ],
+    openGraph: {
+      title: "Ovlašteno montiranje klima uređaja - Našice | Quintus",
+      description:
+        "Ovlašteni monteri klima uređaja u Našicama i Slavoniji. Montaža, servis i redovito održavanje klima uređaja za domove i poslovne prostore.",
+      url: "/usluge/klimatizacija",
+      siteName:
+        "Quintus | Obrt za vodu, plin, grijanje i klimatizaciju - Našice",
+      locale: "hr_HR",
+      type: "website",
+      images: [
+        {
+          url: "https://www.instalacije-quintus.hr/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdzhmn7c4d%2Fimage%2Fupload%2Fv1780157613%2Fquintus_images%2Fzv34ptnsgknm1jookolw.png&w=640&q=75",
+          width: 640,
+          height: 640,
+          alt: "Quintus logo",
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: "Ovlašteno montiranje klima uređaja - Našice | Quintus",
+      description:
+        "Ovlašteni monteri klima uređaja u Našicama i Slavoniji. Montaža, servis i redovito održavanje klima uređaja za domove i poslovne prostore.",
+      images: [
+        "https://www.instalacije-quintus.hr/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdzhmn7c4d%2Fimage%2Fupload%2Fv1780157613%2Fquintus_images%2Fzv34ptnsgknm1jookolw.png&w=640&q=75",
+      ],
+    },
   },
   grijanje: {
     title: "Instalacije i ugradnja grijanja - Našice | Quintus",
@@ -35,6 +62,33 @@ const SERVICE_SEO_OVERRIDES = {
       "posjedujemo sve potrebne certifikate za instalacije grijanja",
       "certificirani instalateri grijanja",
     ],
+    openGraph: {
+      title: "Instalacije i ugradnja grijanja - Našice | Quintus",
+      description:
+        "Ugradnja, servis i održavanje sustava grijanja u Našicama i Slavoniji - centralno grijanje, podno grijanje i toplinske pumpe.",
+      url: "/usluge/grijanje",
+      siteName:
+        "Quintus | Obrt za vodu, plin, grijanje i klimatizaciju - Našice",
+      locale: "hr_HR",
+      type: "website",
+      images: [
+        {
+          url: "https://www.instalacije-quintus.hr/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdzhmn7c4d%2Fimage%2Fupload%2Fv1780157613%2Fquintus_images%2Fzv34ptnsgknm1jookolw.png&w=640&q=75",
+          width: 640,
+          height: 640,
+          alt: "Quintus logo",
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: "Instalacije i ugradnja grijanja - Našice | Quintus",
+      description:
+        "Ugradnja, servis i održavanje sustava grijanja u Našicama i Slavoniji - centralno grijanje, podno grijanje i toplinske pumpe.",
+      images: [
+        "https://www.instalacije-quintus.hr/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdzhmn7c4d%2Fimage%2Fupload%2Fv1780157613%2Fquintus_images%2Fzv34ptnsgknm1jookolw.png&w=640&q=75",
+      ],
+    },
   },
   "voda-i-kanalizacija": {
     title: "Vodoinstalacije i kanalizacija - Našice | Quintus",
@@ -47,6 +101,33 @@ const SERVICE_SEO_OVERRIDES = {
       "posjedujemo sve potrebne certifikate za vodoinstalaterske radove",
       "certificirani vodoinstalateri",
     ],
+    openGraph: {
+      title: "Vodoinstalacije i kanalizacija - Našice | Quintus",
+      description:
+        "Vodoinstalaterski radovi i uređenje kanalizacije u Našicama i Slavoniji - ugradnja, popravci i održavanje vodovodnih instalacija.",
+      url: "/usluge/voda-i-kanalizacija",
+      siteName:
+        "Quintus | Obrt za vodu, plin, grijanje i klimatizaciju - Našice",
+      locale: "hr_HR",
+      type: "website",
+      images: [
+        {
+          url: "https://www.instalacije-quintus.hr/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdzhmn7c4d%2Fimage%2Fupload%2Fv1780157613%2Fquintus_images%2Fzv34ptnsgknm1jookolw.png&w=640&q=75",
+          width: 640,
+          height: 640,
+          alt: "Quintus logo",
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: "Vodoinstalacije i kanalizacija - Našice | Quintus",
+      description:
+        "Vodoinstalaterski radovi i uređenje kanalizacije u Našicama i Slavoniji - ugradnja, popravci i održavanje vodovodnih instalacija.",
+      images: [
+        "https://www.instalacije-quintus.hr/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdzhmn7c4d%2Fimage%2Fupload%2Fv1780157613%2Fquintus_images%2Fzv34ptnsgknm1jookolw.png&w=640&q=75",
+      ],
+    },
   },
 };
 
