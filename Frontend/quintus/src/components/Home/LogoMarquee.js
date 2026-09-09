4c6e9a2bdcd84f48fbcb15e15da3e8b8cd2219a9
Filip Ćurić <filip.curic6@yahoo.com>
2026-09-09 16:32:41 +0200

    Remove toshiba logo

--- Frontend/quintus/src/components/Home/LogoMarquee.js
+++ Frontend/quintus/src/components/Home/LogoMarquee.js
@@ -8,7 +8,7 @@ const manufacturers = [
   // { name: "Samsung", src: "/images/samsung.png", width: 2000, height: 306 },
   { name: "Vaillant", src: "/images/vaillant.png", width: 613, height: 161 },
   { name: "Tesla", src: "/images/tesla.webp", width: 947, height: 232 },
-  { name: "Toshiba", src: "/images/toshiba.png", width: 747, height: 122 },
+  // { name: "Toshiba", src: "/images/toshiba.png", width: 747, height: 122 },
 ];
 
 function LogoGroup({ duplicate = false }) {
