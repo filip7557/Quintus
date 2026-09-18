5c25ce1e7c2a3aa7ec57c00fabf7a8a1332eb66b
Filip Ćurić <filip.curic6@yahoo.com>
2026-09-18 17:03:00 +0200

    Add certificate service

---
+++ Backend/Quintus/Quintus.Service.Common/ICertificateService.cs
@@ -0,0 +1,17 @@
+﻿using Quintus.Model.Entities;
+
+namespace Quintus.Service.Common
+{
+    public interface ICertificateService
+    {
+        Task<IEnumerable<CertificateResponseDTO>> GetAllCertificatesAsync();
+
+        Task<bool> AddCertificateAsync(CertificateDTO certificate);
+
+        Task<bool> UpdateCertificateAsync(CertificateUpdateDTO certificate, Guid id);
+
+        Task<bool> UpdateCertificateImageAsync(Guid id, string imageUrl);
+
+        Task<bool> DeleteCertificateAsync(Guid id);
+    }
+}
---
+++ Backend/Quintus/Quintus.Service/CertificateService.cs
@@ -0,0 +1,60 @@
+﻿using Quintus.Model.Entities;
+using Quintus.Repository.Common;
+using Quintus.Service.Common;
+
+namespace Quintus.Service
+{
+    public class CertificateService : ICertificateService
+    {
+        private readonly IImageService _imageService;
+        private readonly ICertificateRepository _certificateRepository;
+
+        public CertificateService(IImageService imageService, ICertificateRepository certificateRepository)
+        {
+            _imageService = imageService;
+            _certificateRepository = certificateRepository;
+        }
+
+        public async Task<bool> AddCertificateAsync(CertificateDTO certificate)
+        {
+            var imageUrl = await _imageService.AddImageAsync(certificate.Image);
+            if (imageUrl == null) return false;
+            var newCertificate = new Certificate
+            {
+                Title = certificate.Title,
+                Description = certificate.Description,
+                ImageUrl = imageUrl.Url,
+                Url = certificate.Url
+            };
+            return await _certificateRepository.AddCertificateAsync(newCertificate);
+        }
+
+        public async Task<bool> DeleteCertificateAsync(Guid id)
+        {
+            return await _certificateRepository.DeleteCertificateAsync(id);
+        }
+
+        public async Task<IEnumerable<CertificateResponseDTO>> GetAllCertificatesAsync()
+        {
+            return await _certificateRepository.GetAllCertificatesAsync()
+                .ContinueWith(task => task.Result.Select(t => new CertificateResponseDTO
+                {
+                    Id = t.Id,
+                    Title = t.Title,
+                    Description = t.Description,
+                    ImageUrl = t.ImageUrl,
+                    Url = t.Url
+                }));
+        }
+
+        public async Task<bool> UpdateCertificateAsync(CertificateUpdateDTO certificate, Guid id)
+        {
+            return await _certificateRepository.UpdateCertificateAsync(certificate, id);
+        }
+
+        public async Task<bool> UpdateCertificateImageAsync(Guid id, string imageUrl)
+        {
+            return await _certificateRepository.UpdateCertificateImageAsync(id, imageUrl);
+        }
+    }
+}
