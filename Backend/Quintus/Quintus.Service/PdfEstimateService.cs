f76c4ffe77e1758d2b9167c05c6af52d29861417
Filip Ćurić <filip.curic6@yahoo.com>
2026-09-23 00:34:57 +0200

    Introduce pdf generation service for estimate

---
+++ Backend/Quintus/Quintus.Service/PdfEstimateService.cs
@@ -0,0 +1,10 @@
+﻿namespace Quintus.Service
+{
+    public class PdfEstimateService
+    {
+        public async Task<byte[]> GeneratePdfAsync(Guid estimateId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
